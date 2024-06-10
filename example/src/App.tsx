2e54f40f7526d706b5182f9b979d26b6728ca005
Venkatesh <venkatmcajj@gmail.com>
2024-06-10 18:10:49 +0530

    Lint Corrections

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,8 @@
-import React, { useState } from "react";
-import "./app.css";
-import {
-  Verify,
-} from "react-puzzle-captcha";
-import "react-puzzle-captcha/dist/react-puzzle-captcha.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState } from 'react';
+import './app.css';
+import { Verify } from 'react-puzzle-captcha';
+import 'react-puzzle-captcha/dist/react-puzzle-captcha.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 export default function App() {
   const [visible, setVisible] = useState(false);
   const show = () => {
@@ -15,22 +13,34 @@ export default function App() {
   };
   return (
     <div className="col-lg-6 mx-auto">
-      <div className="d-flex my-2 justify-content-center" >
-        {!visible ? <div onClick={show} className="btn btn-success">
-          Show
-        </div> :
+      <div className="d-flex my-2 justify-content-center">
+        {!visible ? (
+          <div onClick={show} className="btn btn-success">
+            Show
+          </div>
+        ) : (
           <div onClick={hide} className="btn btn-danger">
             Hide
-          </div>}
+          </div>
+        )}
       </div>
       <Verify
         width={320}
         height={160}
         visible={visible}
-        onSuccess={() => {alert('success');hide();}}
-        onFail={() => {alert('fail')}}
-        onRefresh={() =>{alert('refresh');}}
-        imgUrl={"https://www.balmerlawrie.com/img/main_images/inside_banner/travel-banner1.jpg"}
+        onSuccess={() => {
+          alert('success');
+          hide();
+        }}
+        onFail={() => {
+          alert('fail');
+        }}
+        onRefresh={() => {
+          alert('refresh');
+        }}
+        imgUrl={
+          'https://www.balmerlawrie.com/img/main_images/inside_banner/travel-banner1.jpg'
+        }
       />
     </div>
   );
